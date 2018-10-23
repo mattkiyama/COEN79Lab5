180b242d890bd490a89b91ffdfafa5f326bcb103
mattkiyama <mattkiyama@gmail.com>
2018-10-23 01:38:49 -0700

    added most member functions without definitions into mystring.cpp

--- Lab5/mystring.cpp
+++ Lab5/mystring.cpp
@@ -9,3 +9,30 @@
 #include <stdio.h>
 #include "mystring.h"
 
+namespace coen79_lab5{
+    // CONSTRUCTOR, COPY CONSTRUCTOR, and DESTRUCTOR
+    string(const char str[ ] = "");
+    string(char c);                     // NEW FUNCTION 1
+    string(const string& source);
+    ~string( );
+    
+    // MODIFICATION MEMBER FUNCTIONS
+    void operator +=(const string& addend);
+    void operator +=(const char addend[ ]);
+    void operator +=(char addend);
+    void reserve(size_t n);
+    string& operator =(const string& source);
+    void insert(const string& source, unsigned int position);
+    void dlt(unsigned int position, unsigned int num);
+    void replace(char c, unsigned int position);
+    void replace(const string& source, unsigned int position);
+    
+    // CONSTANT MEMBER FUNCTIONS
+    size_t length( ) const { return current_length; }
+    char operator [ ](size_t position) const;
+    int search(char c) const;
+    int search(const string& substring) const;
+    unsigned int count(char c) const;
+
+}
+
