589507404967d6e4735ac69592f3d5c3c6c65e0f
mattkiyama <mattkiyama@gmail.com>
2018-10-23 01:25:44 -0700

    Initial Commit

---
+++ Lab5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lab5
+//
+//  Created by Matthew Kiyama on 10/23/18.
+//  Copyright © 2018 SCU. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
